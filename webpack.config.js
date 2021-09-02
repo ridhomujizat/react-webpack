85069ff13a885404526ca4703489ecab68cb0068
Ridho Mujizat <ridhomujizat@gmail.com>
2021-09-02 12:16:48 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: [/\.s[ac]ss$/i, /\.css$/i],
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+        //   "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|jpeg|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    path: path.resolve(__dirname, './src/assets/image'),
+                }
+            },
+        ],
+      }
+    ],
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
